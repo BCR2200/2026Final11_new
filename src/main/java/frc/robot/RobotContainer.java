4b52c0b3e6ba7508ed687dd1e06837d7563c6d40
bcrteammember <bcrteammember@gmail.com>
2026-03-01 11:55:03 -0500

    faster robot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -125,7 +125,7 @@ public class RobotContainer {
   // Thank you! A.T.S.T Also give us an angled floor.
 
   @NotLogged
-  public final static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.2;
+  public final static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.5;
   @NotLogged
   public final static double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
 
