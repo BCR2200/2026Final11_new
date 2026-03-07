c1a839219d2277f5886bcbd82c4fca98b72e2f2d
bcrteammember <bcrteammember@gmail.com>
2026-03-07 08:37:19 -0500

    calculate compensated target hub in tele and auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -336,7 +336,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    m_robotContainer.calculateCompensatedTargetHub();
+  }
 
   @Override
   public void teleopInit() {
