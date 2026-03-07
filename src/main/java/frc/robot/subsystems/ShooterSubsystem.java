5f1719abf3d837ee2945b2f0e959071587ae8b46
bcrteammember <bcrteammember@gmail.com>
2026-03-07 10:51:20 -0500

    Update shooter max rps

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -114,7 +114,7 @@ public class ShooterSubsystem extends SubsystemBase {
      * @param distance in m
      */
     public void setShooterSpeedViaInterpolatedValue(double distance) {
-        this.shooterSpeed = shooterVelocityInterpolator.clampedInterpolate(distance, 0, 110); // In rps
+        this.shooterSpeed = shooterVelocityInterpolator.clampedInterpolate(distance, 0, 95); // In rps
     }
 
     /**
