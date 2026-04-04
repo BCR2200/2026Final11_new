bf412d4b1b1f5697c094c43a8acf76b964165f20
bcrteammember <bcrteammember@gmail.com>
2026-04-04 13:22:04 -0400

    Sweep driver wall changes

--- src/main/java/frc/robot/commands/auto/SweepDriver.java
+++ src/main/java/frc/robot/commands/auto/SweepDriver.java
@@ -5,18 +5,24 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.drive.CommandSwerveDrivetrain;
 
 public class SweepDriver extends AutoCommand {
 
+    private PathPlannerPath pathRight;
+    private PathPlannerPath pathLeft;
     private PathPlannerPath path;
     
     public SweepDriver(RobotContainer robotContainer, CommandSwerveDrivetrain drivetrain, SwerveRequest.RobotCentric swerve) {
-        path = AutoBuildingBlocks.loadPathOrThrow("SweepDriver.1");
+        pathRight = AutoBuildingBlocks.loadPathOrThrow("SweepDriverRight.1");
+        pathLeft = AutoBuildingBlocks.loadPathOrThrow("SweepDriverLeft.1");
+        path = isOnRightSide(drivetrain) ? pathRight : pathLeft;
 
         addCommands(
             new WaitCommand(0.01),
@@ -27,6 +33,25 @@ public class SweepDriver extends AutoCommand {
             ),
             AutoBuildingBlocks.followPathCommand(path, drivetrain)
         );
+    }
+
+    private boolean isOnRightSide(CommandSwerveDrivetrain drivetrain) {
+        if (Robot.alliance == DriverStation.Alliance.Blue) {
+            if (drivetrain.getState().Pose.getY() < 4) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            if (drivetrain.getState().Pose.getY() > 4) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 
     @Override
